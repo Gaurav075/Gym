3635b803a5b4ad96ea76e23df5f17519b7696247
Gaurav075 <125338113+Gaurav075@users.noreply.github.com>
2024-09-07 03:20:08 +0000

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
 import Hero from './components/Hero/Hero';
+import Program from './components/programs/Program';
+import Reasons from './components/Reasons/Reasons';
+import Plans from './components/Plans/Plans';
+import Testimonials from './components/Testimonials/Testimonials';
+import Join from './components/Join/Join';
+import Footer from './components/Footer/Footer';
 function App() {
   return (
     <div className="App">
          <Hero/>
+         <Program/>
+         <Reasons/>
+         <Plans/>
+         <Testimonials/>
+         <Join/>
+         <Footer/>
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.css'
 import Logo from '../../assets/logo.png'
+import Bars from '../../assets/bars.png'
+import {Link} from "react-scroll"
 const Header = () => {
+  const mobile = window.innerWidth<=768 ? true : false
+  const [menuopened,setmenuopened] = useState(false);
   return (
     <div className='Header'>
       <img src={Logo} alt="" className='logo'/>
-      <ul className='header-menu'>
-        <li>Home</li>
-        <li>Programs</li>
-        <li>Why us</li>
-        <li>Plans</li>
-        <li>Testimonials</li>
-      </ul>
+      {(menuopened===false && mobile==true) ? (
+        <div
+        style={{backgroundColor:'var(--appColor)',padding:'0.5rem',borderRadius:"5px"}}
+        onClick={()=>setmenuopened(true)}
+        >
+          
+          <img src={Bars} alt="" style={{width:'1.5rem',height:'1.5rem'}}/></div>
+        )  : <ul className='header-menu'>
+        <li >
+         <Link
+         onClick={()=>setmenuopened(false)}
+         to='home'
+         activeClass='active'
+          span={true}
+          smooth={true}
+         >Home</Link>
+        </li>
+        <li onClick={()=>setmenuopened(false)}>
+          Programs
+        </li>
+        <li onClick={()=>setmenuopened(false)}>
+          Why us
+        </li>
+        <li onClick={()=>setmenuopened(false)}>Plans</li>
+        <li >
+          <Link
+          onClick={()=>setmenuopened(false)}
+          to='testimonials'
+          span={true}
+          smooth={true}
+          >Testimonials</Link>
+        </li>
+      </ul>}
+      
 
     </div>
   )
